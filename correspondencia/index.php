d7c92048b1d3c3ce740d809759b9b8c6545d36bd
Reynaldo <capri.2594@gmail.com>
2026-03-04 00:16:42 -0400

    arreglo de hoja de ruta en casa

--- correspondencia/index.php
+++ correspondencia/index.php
@@ -216,7 +216,7 @@ try {
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Imprimir página de reporte</h5>
+                    <h5 class="modal-title">Indica el número de página de hoja de ruta</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
                 <div class="modal-body">
@@ -230,7 +230,7 @@ try {
                 </div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                    <button class="btn btn-primary" form="printPageForm">Abrir reporte</button>
+                    <button class="btn btn-primary" form="printPageForm">Generar Hoja</button>
                 </div>
             </div>
         </div>
